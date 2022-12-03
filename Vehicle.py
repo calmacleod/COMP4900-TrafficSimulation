657d1fb173a7306a7903ee4fdfced8b4d9240df5
Kyuri Jung <60691290+kyurijung@users.noreply.github.com>
2022-12-03 17:05:33 -0500

    getting the acceleration

--- Vehicle.py
+++ Vehicle.py
@@ -3,8 +3,7 @@ class Vehicle:
         self.road = road
         self.lead = lead
 
-        self.delta_x = lead.x - self.x - lead.l
-        self.delta_y = self.v - lead.v
+        self.delta = 2
         self.x = 0
         self.l = 4
         self.s = self.x - lead.x - self.l
@@ -21,6 +20,6 @@ class Vehicle:
 
     def updatePosition(self):
         sStar = self.s0 + (self.v * self.T) + ((self.v * self.diff_v) / sqrt(2 * self.a * self.b))
-        acc = self.a * (1- (self.v / self.diff_v)^delta_x - (sStar / self.s)^2)
+        acc = self.a * (1- (self.v / self.diff_v)^delta - (sStar / self.s)^2)
         
         return acc
