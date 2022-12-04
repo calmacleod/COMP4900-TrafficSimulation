8d6fe75ef677b52fff7c19060f7465c684108f64
Callum MacLeod <callummacleod@cmail.carleton.ca>
2022-12-04 10:24:56 -0500

    Add car list to road

--- Road.py
+++ Road.py
@@ -3,8 +3,12 @@ class Road:
     def __init__(self, direction, offset):
         self.direction = direction
         self.offset = offset
+        self.vehicles = []
 
     #TODO Implement
     def update(self):
         return 0
+
+    def add_vehicle(self,vehicle):
+        self.vehicles.append(vehicle)
 
